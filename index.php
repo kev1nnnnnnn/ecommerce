b7d90ac86f4d1b331343fae8c5d8486be18984e4
John Kevin <kevinbfv@gmail.com>
2020-08-04 15:11:56 -0400

    CRUD ADM

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $app->get('/admin/login', function() {
 
 });
 
-//validando o LOGIN ************************
+//Rota para o LOGIN ************************
 $app->post('/admin/login', function() {
 
 	User::login($_POST["login"], $_POST["password"]);
@@ -55,12 +55,131 @@ $app->post('/admin/login', function() {
 
 });
 
-$app->get('/admin/logout', function() {
+//Rota para o LOGOUT ************************
+$app->get("/admin/logout", function() {
+
 	User::logout();
+
 	header("Location: /admin/login");
 	exit;
 });
 
-$app->run();
 
- ?>
+$app->get("/admin/users", function() {
+
+	User::verifyLogin();
+
+	$users = User::listAll(); //método estático
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array (
+		"users"=>$users
+	));
+
+});
+
+//Rotas de tela *************************
+
+$app->get("/admin/users/create", function() {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl('users-create');
+
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("location: /admin/users");
+
+	exit;
+
+});
+
+//rota de edição *************************
+$app->get('/admin/users/:iduser', function($iduser){
+ 
+   User::verifyLogin();
+ 
+   $user = new User();
+ 
+   $user->get((int)$iduser);
+ 
+   $page = new PageAdmin();
+ 
+   $page ->setTpl("users-update", array(
+        "user"=>$user->getValues()
+    ));
+ 
+});
+
+//rota pra salvar *************************
+$app->post("/admin/users/create", function() {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0; //se for definido o valor é 1, se nao for o valor é 0
+
+	$user->setData($_POST);
+
+	//executar o insert no banco
+	$user->save();
+
+	header("location: /admin/users");
+	exit;
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("location: /admin/users");
+	exit;
+});
+
+//Rota do FORGOT
+$app->get("/admin/forgot", function() {
+
+	$page = new PageAdmin([
+
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");
+
+});
+
+//pegar o email que o usuário envio via post
+$app->post("/admin/forgot", function() {
+
+	$user = User::getForgot($_POST["email"]);
+});
+
+
+
+$app->run();
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -65,7 +65,79 @@ class User extends Model{
 	{
 		$_SESSION[User::SESSION] = NULL;
 	}
-}
+
+	//MÉTODO LIST ALL
+	public static function listAll()
+	{
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+	}
+
+	//nao pode ser estático para ter acesso as informações no atributo
+
+	public function save() 
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			"nrphone"=>$this->getnrphone(),
+			"inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+	}
+
+
+	public function get($iduser)
+	{
+		 
+	 $sql = new Sql();
+	 
+	 $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser;", array(
+	 ":iduser"=>$iduser
+	 ));
+	 
+	 $data = $results[0];
+	 
+	 $this->setData($data);
+		 
+	}
+
+	//EDITAR USUARIO
+	public function update()
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+
+			":iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			"nrphone"=>$this->getnrphone(),
+			"inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+	}
+
+	//DELETAR USUARIO
+	public function delete()
+	{
+		$sql = new Sql();
+
+		$sql->query("CALL sp_users_delete(:iduser)", array(
+			"iduser"=>$this->getiduser()
+		));
+	}
+}	
 
 
 
